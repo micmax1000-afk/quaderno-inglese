2dffed45ebf8868efe1a721eddb0b3f2ec1fa491
micmax1000-afk <mic.max.1000@gmail.com>
2026-09-24 18:52:43 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='quaderno-inglese-v15-7-mobile';
+const CACHE='quaderno-inglese-v16-tutor';
 const CORE=['./','./index.html','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','./data/oxford3000.js'];
 self.addEventListener('install',event=>event.waitUntil((async()=>{
   const cache=await caches.open(CACHE);
